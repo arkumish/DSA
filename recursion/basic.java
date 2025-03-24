9ec724e0ff472ec11d64c7cdfc2e45113f2e6b55
Arpit Kumar Mishra <31367960+arkumish@users.noreply.github.com>
2025-03-24 09:39:54 +0530

    added recursion

--- recursion/basic.java
+++ recursion/basic.java
@@ -4,5 +4,6 @@ The real feel of recursion is to try Basic
 2: print 1 to n 
 3: print n to 1
 4: reverse an array
+5: do some Tree DFS questions
 
 so simple, if we can solve big problem as small problem, thn we will use recursion
---
+++ recursion/uniqueSubSet.java
@@ -0,0 +1,37 @@
+class Solution {
+    /*
+    print all subset, take, not take method, call 
+
+    base condition : if(index==n)  add array to final array ; return ;
+    addnum
+    //take call
+    //remove last added
+    //not take call
+
+    */
+    private void fxn(int ind, int[] nums, List<List<Integer>> finalArray, List<Integer> currArr ){
+
+        if(ind==nums.length){
+            finalArray.add(new ArrayList<>(currArr)); // keep in mind, as java takes as reference, so directly adding while handle all future values
+            return;
+        }
+        
+        //take
+        currArr.add(nums[ind]);
+        fxn(ind+1,nums, finalArray, currArr);
+        
+        //not take
+        currArr.remove(currArr.size()-1);
+        fxn(ind+1,nums, finalArray, currArr);
+        
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> finalArray = new ArrayList<>();
+
+         fxn(0,nums, finalArray, new ArrayList<>());
+
+         return finalArray;
+
+        
+    }
+}
