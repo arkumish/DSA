33b042cb07891884abe2c8312b8aa11e4a52e264
Arpit Kumar Mishra <31367960+arkumish@users.noreply.github.com>
2025-03-25 18:07:49 +0530

    udpated dp

--- dynamic-programming/basic.java
+++ dynamic-programming/basic.java
@@ -39,6 +39,82 @@ Top to down : u do recursion with storing data somewhere
 bottom up : u do not recursion, but some kind of looping iteration using storing data somewhere
 
 
+eg : fibo
+------------------------------------------------------------------------------
+Top to down : 
+
+public int fxn(int n,int[] dp){
+ 
+ if(n==0) return 0;  //base case
+ if(n==1) return 1;
+ 
+  if(dp[n]!=-1)
+   return dp[n];
+ 
+ dp[n] = fxn(n-1,dp) + fxn(n-2,dp);
+
+ return dp[n];
+
+}
+
+pvsm(){
+
+int[] dp = new int[n+1];
+Arrays.fill(dp, -1);
+
+fxn(n,dp);
+
+return dp[n];
+
+}
+
+----------------------------------------------------------------
+
+Bottom Up :
+
+pvsm(){
+
+ int[] dp = new int[n+1];
+ Arrays.fill(dp,-1);
+
+ dp[0]=0;
+ dp[1]=1;
+
+ for(int i=2;i<=n;i++)
+  dp[i] = dp[i-1] + dp[i-2];
+
+return dp[n]
+
+
+-----------------------------------------------------------
+
+for improving theese (space complexity), u can try 3 variable 
+
+public static int fxn(int n) {
+    if (n == 0) return 1;
+    if (n == 1) return 0;
+
+    int prev2 = 1, prev1 = 0, curr = 0;
+
+    for (int i = 2; i <= n; i++) {
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return curr;
+}
+
+ ------------------------------------------------------------
+
+  HAPPY CODING
+
+  -------------------------------------------
+
+
+}
+
+
 
 
 */
